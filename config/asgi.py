6977bd9a3eed8a2a723f2b65867e7757a3da8ed2
Akhmadjon Akbarov <97084545+akhmadjonakbarov@users.noreply.github.com>
2023-04-06 22:22:16 +0500

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,6 +1,6 @@
 # mysite/asgi.py
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
@@ -10,7 +10,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
-
+django.setup()
 import chatsapp.routing
 
 application = ProtocolTypeRouter(
